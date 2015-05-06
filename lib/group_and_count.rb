4b679cc4f467d6ea6aaad154d1fe32f85ef3abfc
Jeffrey Barszcz <jfbarszcz@gmail.com>
2015-05-06 19:32:18 -0400

    done?

--- lib/group_and_count.rb
+++ lib/group_and_count.rb
@@ -1,7 +1,13 @@
 class Hash
-
   def hmap(&prc)
-    self.map { |k, v| [k, prc.call(v)] }
-    .to_h
+    map { |k, v| [k, prc.call(v)] }.to_h
+  end
+end
+
+class Array
+  def group_and_count(keys)
+    return size if keys.empty?
+    grouped = group_by { |el| el[keys.first] }
+    grouped.hmap { |subarr| subarr.group_and_count(keys.drop(1)) }
   end
 end
--- spec/group_and_count_spec.rb
+++ spec/group_and_count_spec.rb
@@ -31,9 +31,71 @@ describe Array do
       ]
     end
 
+    let(:counted_events) do
+      {
+        "bill.split" => {
+          "Boston" => {
+            "morning" => 1,
+            "evening" => 1
+            },
+            "Chicago" =>  {
+              "evening" => 1
+            }
+            },
+            "promo.used" => {
+              "New York" => { 
+                "afternoon" => 2 
+              }
+            }
+          }
+        end
+
+    let(:events_keys) { [:event, :city, :time_of_day] }
+
     it "returns a simple count when no keys are given" do
       counted = events.group_and_count([])
       expect(counted).to eq 5
+    end
+
+    it "returns properly grouped and counted results" do
+      gc = events.group_and_count(events_keys)
+      expect(gc).to eq(counted_events)
+    end
+
+    let(:heterogeneous) do
+      [
+        { a: 1, b: 2, c: 5, d: 8},
+        { a: 1, b: 2, d: 8 },
+        { a: 1, b: 2, d: 7, foobar: 22 },
+        { a: 1, b: 3, c: 47, d: 8, idunnolol: "I dunno lol" },
+        { a: 3, b: 4, d: 2, name: "Lil B", based_level: 31 }
+      ]
+    end
+
+    let(:counted_hetero) do
+      {
+        1 => {
+          2 => {
+            7 => 1,
+            8 => 2
+            },
+            3 => {
+              8 => 1
+            }
+            },
+            3 => {
+              4 => {
+                2 => 1
+              }
+            }
+          }
+        end
+
+    let(:hetero_keys) { [:a, :b, :d] }
+
+    it "doesn't choke on heterogeneous record structures" do
+      gc = heterogeneous.group_and_count(hetero_keys)
+      expect(gc).to eq(counted_hetero)
     end
 
   end
